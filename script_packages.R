6b3e487ec3d93dbf0d2c936920bd207c508fc867
marc.agenis-nevers@veolia.com <marc.agenis-nevers@veolia.com>
2017-02-14 07:44:35 +0000

    Upload new file

---
+++ script_packages.R
@@ -0,0 +1,19 @@
+
+#######################################
+# PACKAGES A CHARGER
+
+# si les packages ne sont pas installes utiliser l'instruction:
+# install.packages("...nom package...")
+# autant de fois que necessaire
+
+library(forecast)
+library(dplyr)
+library(zoo)
+library(ggplot2)
+library(gridExtra)
+library(lubridate)
+library(fields)
+library(gtrendsR)
+library(magrittr)
+
+
